fd1a27b9e729584f95c67f2e4f14a168e576bbc5
Joshua Bartholomew <joshua.bartholomew@realtruck.com>
2022-03-10 22:26:02 -0600

    Update Install-ANGSoftware.ps1
    Added functionality and notes.

--- Install-ANGSoftware.ps1
+++ Install-ANGSoftware.ps1
@@ -1,4 +1,4 @@
 choco install installroot -y
 choco install vmware-horizon-client -y
 
-# 2022-03-10: As far as I can tell, this script sets up the necessary components for remote work with the Air Guard.
+# 2022-03-10: One thing this script doesn't do is configure the VMware Horizon client with the AFRC Desktop Anywhere server settings. But, it seems that installroot was able to make the DoD root certificates available to Horizon.
