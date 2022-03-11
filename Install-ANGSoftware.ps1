cc613566ff39c1bd4a38d680db02241c2e34bc32
Joshua Bartholomew <joshua.bartholomew@realtruck.com>
2022-03-10 22:17:27 -0600

    Create Install-ANGSoftware.ps1
    Added script for installing Air Guard remote work software.

---
+++ Install-ANGSoftware.ps1
@@ -0,0 +1,4 @@
+choco install installroot -y
+choco install vmware-horizon-client -y
+
+# 2022-03-10: As far as I can tell, this script sets up the necessary components for remote work with the Air Guard.
