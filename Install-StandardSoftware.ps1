3e515f0b6b5904455731ac0655189460b678ad9c
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2022-03-30 22:41:39 -0500

    Added some more software to the standard list.

--- Install-StandardSoftware.ps1
+++ Install-StandardSoftware.ps1
@@ -1,3 +1,13 @@
 choco install bitwarden -y
 choco install bitwarden-cli -y
 choco install discord -y
+choco install vlc
+choco install notepadplusplus
+choco install microsoft-teams
+choco install jasp
+choco install icue
+choco install googledrive
+choco install GoogleChrome
+choco install Firefox
+choco install anki
+choco install adobereader
