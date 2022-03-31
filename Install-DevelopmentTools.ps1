bb10c39c5dca7cc28bd692c4190165aca7350afe
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2022-03-30 21:39:29 -0500

    Added basic installation script for dev tools.

---
+++ Install-DevelopmentTools.ps1
@@ -0,0 +1,12 @@
+# Install and configure Git
+choco install git
+git config --global user.name "Joshua Bartholomew"
+git config --global user.email "joshua.c.bartholomew@gmail.com"
+
+# Install VS Code
+choco install vscode
+
+# Install components for Docker - used for application testing
+choco install wsl2
+choco install wsl-ubuntu-2004
+choco install docker-desktop
