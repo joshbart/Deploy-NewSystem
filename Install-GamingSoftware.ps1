1d9a9903f66a53a0132df5b8ef74eb6673c565fb
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2022-04-15 11:40:04 -0500

    Automatically configures DOSBox.

--- Install-GamingSoftware.ps1
+++ Install-GamingSoftware.ps1
@@ -1,2 +1,22 @@
 choco install steam-client
+
+# Installs and configures DOSBox for retro PC gaming.
 choco install dosbox
+
+## Variables must be set after DOSBox is installed.
+$DBoxAppdataDir = "$env:USERPROFILE\AppData\Local\DOSBox"
+$DBoxConfigFilename = Get-ChildItem $DBoxAppdataDir -Name
+$ScriptWorkingDir = Get-Location
+
+## TODO: Create a game directory in AppData.
+## TODO: Create a module to automatically pull games into AppData directory from internet location.
+
+## TODO: Pull the template conf file from the GitHub repo instead of locally.
+Write-Output "Copying DOSBox config to AppData Directory..."
+Copy-Item -Path $ScriptWorkingDir\dosbox-template.conf -Destination $DBoxAppdataDir
+
+Write-Output "Renaming DOSBox config file..."
+Remove-Item -Path $DBoxAppdataDir\$DBoxConfigFilename
+Rename-Item -Path $DBoxAppdataDir\dosbox-template.conf -NewName $DBoxConfigFilename
+
+Write-Output "DOSBox successfully installed."
