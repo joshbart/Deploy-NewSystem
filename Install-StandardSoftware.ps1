dd686d0b89ff9cba914ea182297a6a75eeb518c3
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2022-05-30 20:45:19 -0500

    Included Balena Etcher in stardard tools.

--- Install-StandardSoftware.ps1
+++ Install-StandardSoftware.ps1
@@ -12,6 +12,7 @@ choco install Firefox
 choco install anki
 choco install adobereader
 choco install filezilla
+choco install etcher
 
 # TODO: Look up Discord options to prevent the installation of a desktop shortcut.
 # TODO: Look up Google Drive options to prevent the installation of Google Apps shortcuts.
