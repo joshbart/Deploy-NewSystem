8864284d954436277c072ba18ec7b1bb4ab743fb
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2022-06-13 08:13:09 -0500

    Updated notes and paint.net installation.

--- Install-StandardSoftware.ps1
+++ Install-StandardSoftware.ps1
@@ -13,6 +13,10 @@ choco install anki
 choco install adobereader
 choco install filezilla
 choco install etcher
+choco install paint.net
 
 # TODO: Look up Discord options to prevent the installation of a desktop shortcut.
 # TODO: Look up Google Drive options to prevent the installation of Google Apps shortcuts.
+# TODO: Configure Google Chrome to use HTTPS by default - https://www.eff.org/https-everywhere/set-https-default-your-browser
+# TODO: Configure Firefox to use HTTPS by default - https://www.eff.org/https-everywhere/set-https-default-your-browser
+# TODO: Add the ability to install Verbum using Chocolatey
